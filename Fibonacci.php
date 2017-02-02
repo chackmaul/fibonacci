4ee16c4e8899cdd578a54f7f42b854ee7b9db9b2
chackmaul <chackmaul@gmail.com>
2017-02-02 15:21:53 -0600

    Created functions to generate a Fibonacci sequence

--- Fibonacci.php
+++ Fibonacci.php
@@ -1,5 +1,33 @@
 <?php
-class Fibonacci 
+namespace Fibonacci;
+use IFibonacci\iFibonacci;
+class Fibonacci implements iFibonacci
 {
-   
+   private $firstValue;
+   private $validatedValue;
+   /**
+    * Fibonacci constructor.
+    * @param mixed $value
+    */
+   public function __construct($value)
+   {
+   }
+   /**
+    *getFibonacci obtain the Fibonacci sequence for a given number and prints it
+    */
+   public function getFibonacci()
+   {
+   }
+   private function setFirst($firstValue)
+   {
+   }
+   private function getFirst()
+   {
+   }
+   private function getNext($actual, $next)
+   {
+   }
+   private function validateValue($value)
+   {
+   }
 }
